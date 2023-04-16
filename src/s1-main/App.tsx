569af096ce38f6be3655c0436a08d0b4650a4366
Serhii <875dfsb6435278785@gmail.com>
2023-04-16 23:26:15 +0200

    it-kamasutra

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import s from './App.module.css'
 import HW1, {message0} from '../s2-homeworks/hw01/HW1'
 import Message, {MessagePropsType} from "../s2-homeworks/hw01/message/Message";
+import HW2 from "../s2-homeworks/hw02/HW2";
 
 function App() {
     return (
         <div className={s.App}>
-            <HW1/>
-            {/*<HW2/>*/}
+            {/*<HW1/>*/}
+            <HW2/>
             {/*<HW3/>*/}
             {/*<HW4/>*/}
 
