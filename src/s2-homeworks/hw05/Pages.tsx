5992adcf4ad3bd0db0c877ac48f12c248a79e005
Serhii <875dfsb6435278785@gmail.com>
2023-05-17 19:44:25 +0200

    hw_5

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import {Routes, Route, Navigate, Path} from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
 import JuniorPlus from './pages/JuniorPlus'
+import path from "path";
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
@@ -18,14 +19,14 @@ function Pages() {
             <Routes>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                {/*<Route ...*/}
+                {/*<Route path='/' element={<PreJunior/>}*/}
 
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
+                <Route path='/pre-junior' element={<PreJunior/>} />
+                <Route path='/junior' element={<Junior/>}/>
+                <Route path='/junior-plus' element={<JuniorPlus/>}/>
 
-                {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
+                {/*роут для несуществующей страницы должен отрисовать*/} <Route element={<Error404 />} />
                 {/*<Route ...*/}
             </Routes>
         </div>
