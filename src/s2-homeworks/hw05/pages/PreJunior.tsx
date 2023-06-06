f8d46b3c17f1bcf03477665cc90ba23bc7c8826f
Serhii <875dfsb6435278785@gmail.com>
2023-06-06 21:43:38 +0200

    hw6

--- src/s2-homeworks/hw05/pages/PreJunior.tsx
+++ src/s2-homeworks/hw05/pages/PreJunior.tsx
@@ -3,6 +3,7 @@ import HW1 from '../../hw01/HW1'
 import HW2 from '../../hw02/HW2'
 import HW3 from '../../hw03/HW3'
 import HW4 from '../../hw04/HW4'
+import HW6 from "../../hw06/HW6";
 
 function PreJunior() {
     return (
