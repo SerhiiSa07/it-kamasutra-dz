cb2cece6574b1569b6eb221fdbb3ccd3ad369c9c
Serhii <875dfsb6435278785@gmail.com>
2023-12-13 22:02:37 +0100

    test8.1

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -12,45 +12,70 @@ import User from './User'
 * */
 
 export type UserType = {
+
     _id: number
     name: string
     age: number
+
 }
 
 const initialPeople: UserType[] = [
+
     // студенты могут поменять имя/возраст/количество объектов, _id должны быть целочисленные
+
     {_id: 0, name: 'Кот', age: 3},
     {_id: 1, name: 'Александр', age: 66},
     {_id: 2, name: 'Коля', age: 16},
     {_id: 3, name: 'Виктор', age: 44},
     {_id: 4, name: 'Дмитрий', age: 40},
     {_id: 5, name: 'Ирина', age: 55},
+
 ]
 
 const HW8 = () => {
+
     const [people, setPeople] = useState<UserType[]>(initialPeople)
     const [currentSort, setCurrentSort] = useState('')
 
     const finalPeople = people.map((u: UserType) => <User key={u._id} u={u}/>)
 
     const sortUp = () => {
+
         setPeople(
+
             homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'})
-        ) // в алфавитном порядке a.name > b.name
+
+        )
+
+        // в алфавитном порядке a.name > b.name
+
         setCurrentSort('up')
+
     }
 
     const sortDown = () => {
+
         setPeople(
+
             homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'})
+
         ) // в обратном порядке a.name < b.name}
+
         setCurrentSort('down')
+
     }
     const check18 = () => {
+
         setPeople(
+
             homeWorkReducer(initialPeople, {type: 'check', payload: 18})
-        ) // совершеннолетние
+
+        )
+
+        // совершеннолетние
+
         setCurrentSort('18')
+
     }
 
     return (
@@ -92,6 +117,7 @@ const HW8 = () => {
 
                         <tbody>{finalPeople}</tbody>
                     </table>
+
                 </div>
             </div>
         </div>
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,25 @@
 import {UserType} from '../HW8'
+import {type} from "os";
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType, action: any): ActionType => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
 
-            return state // need to fix
+        case 'sort': {
+
+            // by name
+
+            return {
+                ...state, name: action.name
+            }  // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return {...state, age: action.age} // need to fix
+
         }
         default:
             return state
